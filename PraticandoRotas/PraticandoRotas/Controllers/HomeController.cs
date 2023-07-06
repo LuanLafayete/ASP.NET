5c5017d920a38b49d596900d0dc76edf9b80a95b
Luã <luanlafayete@gmail.com>
2023-07-06 00:56:29 -0300

    Update route

---
+++ PraticandoRotas/PraticandoRotas/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaDemo.Models;
+using System.Diagnostics;
+
+namespace MinhaDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
