c37eb90f3ae97d535fe4d3464e37fa69443cdac2
Luã <luanlafayete@gmail.com>
2023-07-07 14:30:11 -0300

    Model

---
+++ PraticandoRotas/PraticandoRotas/Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace PraticandoRotas.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+
+    }
+}
