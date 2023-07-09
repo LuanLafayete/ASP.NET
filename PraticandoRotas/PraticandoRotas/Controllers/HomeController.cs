ba850797004e0ed8fef8b1aa8f7f80a589798042
Luã <luanlafayete@gmail.com>
2023-07-09 03:19:12 -0300

    Praticando VIEWS

--- PraticandoRotas/PraticandoRotas/Controllers/HomeController.cs
+++ PraticandoRotas/PraticandoRotas/Controllers/HomeController.cs
@@ -10,22 +10,43 @@ namespace PraticandoRotas.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var filme = new Filme
+            {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avalicao = 10,
+                Valor = 20000
+            };
+
+            return RedirectToAction("Privacy", filme);
+            //return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy(Filme filme)
+        {           
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+            return View();
+
+
+
             //return View(); **** AQUI ELE ESTÁ RETORNANDO AS INFORMAÇÕES DO PRIVACY QUE ESTÃO CONTIDAS NA PASTA VIEW
 
             //return Json("{'nome':'Eduardo'}"); AQUI ELE ESTÁ RETORNANDO UM AQUIVO JSON COM AS INFORMAÇÕES DO PARÂMETRO
 
-            /* AQUI ELE TERIA QUE ABRIR UM ARQUIVO EM TEXTO
+            /* AQUI ELE TERIA QUE ABRIR UM ARQUIVO EM TEXTO SE O LINK DE ACESSO À PASTA ESTIVESSE CERTO
              var fileBytes = System.IO.File.ReadAllBytes(@"F:\arquivo.txt");
             var fileName = "Ola.txt";
             return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
             */
-
-            return Content ("Qualquer coisa");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ PraticandoViews/PraticandoViews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PraticandoViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
