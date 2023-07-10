c5a7e8c0744d802c2d02975ab36280441640af6b
Luã <luanlafayete@gmail.com>
2023-07-10 15:56:31 -0300

    View Components

---
+++ PraticandoViewComponents/PraticandoViewComponents/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PraticandoViewComponents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PraticandoViewComponents/PraticandoViewComponents/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PraticandoViewComponents.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
