bdeea5a1237259a1a2ee28ce2c0a26158659524c
Luã <luanlafayete@gmail.com>
2023-07-22 19:09:09 -0300

    TagHelper

---
+++ AppModelo/src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DevIO.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper 
+    {
+        public string DominioEmail { get; set; } = "knockknockenglish.com.br";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + DominioEmail;
+            output.Attributes.SetAttribute("href", "malito:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
